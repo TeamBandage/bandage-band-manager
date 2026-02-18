9f606af1754ff0f6b574116ce1ba16ab2f751cc3
willjsw <sunwoo1137@gmail.com>
2026-02-19 02:01:31 +0900

    feat: Practice 및 연관 엔티티 구현
    - Practice/PracticeParticipant/PracticeSession/PracticeSong 엔티티 구현
    - SessionType 엔티티 구현
    - .gitkeep 삭제
    
    #1

---
+++ src/main/kotlin/com/bandage/v1/domain/practice/model/PracticeSong.kt
@@ -0,0 +1,50 @@
+package com.bandage.v1.domain.practice.model
+
+import com.bandage.v1.global.domain.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.Id
+import jakarta.persistence.OneToOne
+import jakarta.persistence.Table
+import org.hibernate.annotations.UuidGenerator
+import java.util.*
+
+@Entity
+@Table(name = "p_practice_song")
+class PracticeSong(
+    @OneToOne(fetch = FetchType.LAZY)
+    @Column(name = "practice_id")
+    val practice: Practice,
+    @Column(name = "title", nullable = false)
+    var title: String,
+    @Column(name = "artist", nullable = false)
+    var artist: String,
+    @Column(name = "album", nullable = false)
+    var album: String,
+    @Column(name = "duration", nullable = false)
+    var duration: Int,
+    @Column(name = "ref_link", nullable = true)
+    var refLink: String? = null
+): BaseEntity() {
+    @Id
+    @Column(name = "practice_song_id")
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    var id: UUID? = null
+
+    companion object {
+        fun create(practice: Practice, title: String, artist: String, album: String, duration: Int): PracticeSong{
+            return PracticeSong(
+                practice = practice,
+                title = title,
+                artist = artist,
+                album = album,
+                duration = duration
+            )
+        }
+    }
+
+    fun updateRefLink(refLink: String){
+        this.refLink  = refLink
+    }
+}
