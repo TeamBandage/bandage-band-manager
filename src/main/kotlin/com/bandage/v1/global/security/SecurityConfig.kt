83fc2a1f5947882fd038188f3339b088a8b1c8ab
willjsw <sunwoo1137@gmail.com>
2026-03-03 09:19:59 +0900

    feat: JwtAuthentication Filter
    - JwtAuthenticationFilter 구현
    - SecurityConfig: JWT 인증 필터 등록
    
    #32

--- src/main/kotlin/com/bandage/v1/global/security/SecurityConfig.kt
+++ src/main/kotlin/com/bandage/v1/global/security/SecurityConfig.kt
@@ -1,19 +1,26 @@
 package com.bandage.v1.global.security
 
+import com.bandage.v1.global.security.filter.JwtAuthenticationFilter
+import com.bandage.v1.global.security.jwt.JwtProvider
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfig {
+class SecurityConfig(
+    private val jwtProvider: JwtProvider,
+) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
             .authorizeHttpRequests { auth ->
                 auth
@@ -25,8 +32,7 @@ class SecurityConfig {
                     ).permitAll()
                     .anyRequest()
                     .authenticated()
-            }.formLogin { it.disable() }
-            .httpBasic { it.disable() }
+            }.addFilterBefore(JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter::class.java)
 
         return http.build()
     }
