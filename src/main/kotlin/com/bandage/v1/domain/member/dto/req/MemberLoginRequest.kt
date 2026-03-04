d414e3cd4e5e6632c5c12abfa0c1bc7727733ff3
willjsw <sunwoo1137@gmail.com>
2026-03-04 11:12:40 +0900

    feat: member join 구현
    - member join 구현
    - Swagger 문서화
    - (refactor) ApiResponse 공통 응답: timestamp 필드 추가
    
    #33

--- src/main/kotlin/com/bandage/v1/domain/member/dto/req/MemberLoginRequest.kt
+++ src/main/kotlin/com/bandage/v1/domain/member/dto/req/MemberLoginRequest.kt
@@ -1,11 +1,12 @@
 package com.bandage.v1.domain.member.dto.req
 
 import io.swagger.v3.oas.annotations.media.Schema
+import jakarta.validation.constraints.NotBlank
 
 @Schema(description = "회원 로그인 요청")
 data class MemberLoginRequest(
-    @Schema(description = "회원 이메일", example = "member@google.com")
+    @NotBlank @Schema(description = "회원 이메일", example = "member@google.com")
     val email: String,
-    @Schema(description = "회원 비밀번호", example = "pw1234")
+    @NotBlank @Schema(description = "회원 비밀번호", example = "pw1234")
     val password: String,
 )
---
+++ src/main/kotlin/com/bandage/v1/domain/member/service/MemberService.kt
@@ -0,0 +1,43 @@
+package com.bandage.v1.domain.member.service
+
+import com.bandage.v1.domain.member.dto.req.MemberJoinRequest
+import com.bandage.v1.domain.member.dto.res.MemberResponse
+import com.bandage.v1.domain.member.model.Member
+import com.bandage.v1.domain.member.repository.MemberRepository
+import com.bandage.v1.global.error.errorcode.ErrorCode
+import com.bandage.v1.global.error.exception.BusinessException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+@Transactional(readOnly = true)
+class MemberService(
+    private val memberRepository: MemberRepository,
+    private val passwordEncoder: BCryptPasswordEncoder,
+) {
+    @Transactional
+    fun createMember(request: MemberJoinRequest): MemberResponse {
+        validateMemberJoin(request)
+        return MemberResponse.of(
+            memberRepository.save(
+                Member.create(
+                    email = request.email,
+                    password = encodePassword(request.password),
+                    name = request.name,
+                    contact = request.contact,
+                ),
+            ),
+        )
+    }
+
+    private fun validateMemberJoin(request: MemberJoinRequest) {
+        if (memberRepository.existsMemberByEmail(request.email)) {
+            throw BusinessException(ErrorCode.DUPLICATE_EMAIL)
+        }
+    }
+
+    private fun encodePassword(rawPassword: String): String =
+        passwordEncoder.encode(rawPassword)
+            ?: throw BusinessException(ErrorCode.INTERNAL_SERVER_ERROR)
+}
