04d33f30e0045516687bcbef048ecb369bfbb48c
willjsw <sunwoo1137@gmail.com>
2026-03-08 18:50:15 +0900

    feat: member audit 구현
    - MemberAuditorAware 구현
    - 비동기 메세지용 AuditContextHolder 구현
    - JpaConfig, BaseTimeEntity -> EntityListeners 등록 통한 JPA Auditing 활성화
    
     #34

--- src/main/kotlin/com/bandage/v1/global/common/domain/BaseTimeEntity.kt
+++ src/main/kotlin/com/bandage/v1/global/common/domain/BaseTimeEntity.kt
@@ -1,11 +1,14 @@
 package com.bandage.v1.global.common.domain
 
 import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.MappedSuperclass
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 
+@EntityListeners(AuditingEntityListener::class)
 @MappedSuperclass
 open class BaseTimeEntity {
     @CreatedDate
